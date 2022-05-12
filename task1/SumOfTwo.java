0f44d3470883f559af767314900a6f90baea9130
Ruslan Omalaev <89082306+Starbreaker84@users.noreply.github.com>
2022-05-12 12:18:45 +0300

    Sum of two linked lists function
    The implementation of a function that receives two linked lists as input, consisting of integer values, and if their lengths are equal, returns a list, each element of which is equal to the sum of the corresponding elements of the input lists.

---
+++ task1/SumOfTwo.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class SumOfTwo {
+    public static List<Integer> sumOfTwo(LinkedList list1, LinkedList list2) {
+        List<Integer> list = new ArrayList<>();
+        if (list1.count() != list2.count()) {
+            return list;
+        }
+        if ((list1.count() == 0) && (list2.count() == 0)) {
+            return list;
+        }
+        Node node1 = list1.head;
+        Node node2 = list2.head;
+        while (node1 != null) {
+            list.add(node1.value + node2.value);
+            node1 = node1.next;
+            node2 = node2.next;
+        }
+        return list;
+    }
+}
