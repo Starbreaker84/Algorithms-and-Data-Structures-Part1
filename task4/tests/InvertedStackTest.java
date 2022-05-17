fac52b3daffdf1b895e3ef942ca3e4309945c436
Ruslan Omalaev <89082306+Starbreaker84@users.noreply.github.com>
2022-05-17 17:03:10 +0300

    Add files via upload

---
+++ task4/tests/InvertedStackTest.java
@@ -0,0 +1,62 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class InvertedStackTest {
+    InvertedStack stack = new InvertedStack();
+
+    @Test
+    void size_empty(){
+        assertEquals(0, stack.size());
+    }
+
+    @Test
+    void size_normal(){
+        stack.stack.add(5);
+        stack.stack.add("five");
+        assertEquals(2, stack.size());
+    }
+
+    @Test
+    void pop_normal(){
+        stack.stack.add(5);
+        stack.stack.add("five");
+        assertEquals(2, stack.size());
+        assertEquals(5, stack.pop());
+        assertEquals("five", stack.pop());
+        assertEquals(0, stack.size());
+    }
+
+    @Test
+    void pop_null(){
+        stack.stack.add(5);
+        stack.stack.add("five");
+        assertEquals(2, stack.size());
+        assertEquals(5, stack.pop());
+        assertEquals("five", stack.pop());
+        assertEquals(0, stack.size());
+        assertNull(stack.pop());
+    }
+
+    @Test
+    void push(){
+        stack.push(5);
+        stack.push("five");
+        assertEquals(2, stack.size());
+        assertEquals("five", stack.stack.get(0));
+        assertEquals(5, stack.stack.get(1));
+    }
+
+    @Test
+    void peek_normal(){
+        stack.stack.add(5);
+        stack.stack.add("five");
+        assertEquals(2, stack.size());
+        assertEquals(5, stack.peek());
+        assertEquals(2, stack.size());
+    }
+
+    @Test
+    void peek_null(){
+        assertNull(stack.peek());
+    }
+}
