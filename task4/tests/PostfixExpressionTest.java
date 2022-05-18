4a592a81365304f349b81be2caf5807ef3ce7b52
Ruslan Omalaev <89082306+Starbreaker84@users.noreply.github.com>
2022-05-18 13:08:43 +0300

    Delete hard test as useless

--- task4/tests/PostfixExpressionTest.java
+++ task4/tests/PostfixExpressionTest.java
@@ -27,21 +27,4 @@ class PostfixExpressionTest {
         stack.push(8);
         assertEquals(59, postfix(stack));
     }
-
-    @Test
-    void postfix_hard(){
-        Stack stack = new Stack();
-        stack.push('=');
-        stack.push('+');
-        stack.push(9);
-        stack.push('*');
-        stack.push(2);
-        stack.push(5);
-        stack.push('+');
-        stack.push(2);
-        stack.push(8);
-        stack.push(3);
-        assertEquals(139, postfix(stack));
-    }
-
 }
