7247330fdabde493d89b4af5670bace58d2a9ca7
Ruslan Omalaev <89082306+Starbreaker84@users.noreply.github.com>
2022-05-25 13:20:47 +0300

    Update PowerSet.java

--- task10/PowerSet.java
+++ task10/PowerSet.java
@@ -1,23 +1,44 @@
 public class PowerSet {
-    public int count;
-    public int step;
-    public int size;
-    public String [] slots;
+    private int count;
+    private int step;
+    private String [] slots;
 
     public PowerSet() {
         count = 0;
-        step = 7;
-        size = 20000;
-        slots = new String[size];
+        step = 3;
+        slots = new String[20000];
     }
 
-    public int hashFun(String key) {
-        if (key == "") return 0;
+    private int hashFun(String key) {
         int sumOfChars = 0;
         for (int i = 0; i < key.length(); i++){
             sumOfChars += key.charAt(i) / 10;
         }
-        return sumOfChars % this.size;
+        return sumOfChars % this.slots.length;
+    }
+
+    private int seek(String value){
+        int slot = hashFun(value);
+        int counter = 0;
+        while (counter < this.slots.length) {
+            if (slot >= this.slots.length) slot = slot - this.slots.length;
+            if (this.slots[slot] == null) return slot;
+            slot += step;
+            counter++;
+        }
+        return -1;
+    }
+
+    private int find(String value){
+        int slot = hashFun(value);
+        int counter = 0;
+        while (counter < this.slots.length) {
+            if (slot >= this.slots.length) slot = slot - this.slots.length;
+            if (this.slots[slot] != null && this.slots[slot].equals(value)) return slot;
+            slot += step;
+            counter++;
+        }
+        return -1;
     }
 
     public int size() {
@@ -25,120 +46,53 @@ public class PowerSet {
     }
 
     public void put(String value) {
-        int slot = this.hashFun(value);
-        boolean inDict = this.get(value);
-        int counter = 0;
-        while (counter < this.size && !inDict){
-            if (slot >= this.size) slot = slot - this.size;
-            if (this.slots[slot] == null) {
-                this.slots[slot] = value;
-                this.count++;
-                break;
-            }
-            slot += step;
-            counter++;
+        if (!get(value)) {
+            slots[this.seek(value)] = value;
+            count++;
         }
     }
 
     public boolean get(String value) {
-        int slot = hashFun(value);
-        int counter = 0;
-        int match = 0;
-        while (counter < this.size && match < count){
-            if (slot >= this.size) slot = slot - this.size;
-            if (this.slots[slot] == value) return true;
-            if (this.slots[slot] != value && this.slots[slot] != null) match++;
-            slot += step;
-            counter++;
-        }
-        return false;
+        return this.find(value) >= 0;
     }
 
     public boolean remove(String value) {
-        int slot = this.hashFun(value);
-        boolean inDict = this.get(value);
-        int counter = 0;
-        while (counter < this.size && inDict){
-            if (slot >= this.size) slot = slot - this.size;
-            if (this.slots[slot] == value) {
-                this.slots[slot] = null;
-                this.count--;
-                return true;
-            }
-            slot += step;
-            counter++;
+        if (get(value)) {
+            slots[find(value)] = null;
+            count--;
+            return true;
         }
         return false;
     }
 
     public PowerSet intersection(PowerSet set2) {
         PowerSet newSet = new PowerSet();
-        int length = this.size();
-        if (length > set2.size()) length = set2.size();
-        int counter = 0;
-        int match = 0;
-        while (counter < this.size && match < length) {
-            if (this.slots[counter] != null && set2.get(this.slots[counter])) {
-                newSet.put(slots[counter]);
-                match++;
-            }
-            counter++;
+        for (String slot : this.slots) {
+            if (slot != null && set2.get(slot)) newSet.put(slot);
         }
         return newSet;
     }
 
     public PowerSet union(PowerSet set2) {
         PowerSet newSet = new PowerSet();
-        int length = this.size();
-        int counter = 0;
-        int match = 0;
-        while (counter < this.size && match < length) {
-            if (this.slots[counter] != null) {
-                newSet.put(this.slots[counter]);
-                match++;
-            }
-            counter++;
-        }
-
-        length = set2.size();
-        counter = 0;
-        match = 0;
-        while (counter < set2.size && match < length) {
-            if (set2.slots[counter] != null) {
-                newSet.put(set2.slots[counter]);
-                match++;
-            }
-            counter++;
+        for (int i = 0; i < this.slots.length; i++) {
+            if (this.slots[i] != null) newSet.put(this.slots[i]);
+            if (set2.slots[i] != null) newSet.put(set2.slots[i]);
         }
         return newSet;
     }
 
     public PowerSet difference(PowerSet set2) {
         PowerSet newSet = new PowerSet();
-        int length = this.size();
-        int counter = 0;
-        int match = 0;
-        while (counter < this.size && match < length) {
-            if (this.slots[counter] != null) match++;
-            if (this.slots[counter] != null && !set2.get(this.slots[counter])) {
-                newSet.put(slots[counter]);
-            }
-            counter++;
+        for (String slot : slots) {
+            if (slot != null && !set2.get(slot)) newSet.put(slot);
         }
         return newSet;
     }
 
     public boolean isSubset(PowerSet set2) {
-        if (this.size() < set2.size()) return false;
-        int length = set2.size();
-        int counter = 0;
-        int match = 0;
-        while (counter < this.size && match < length) {
-            if (set2.slots[counter] != null) match++;
-            if (set2.slots[counter] != null && !this.get(set2.slots[counter])) {
-                return false;
-            }
-            counter++;
+        for (int i = 0; i < set2.slots.length; i++) {
+            if (set2.slots[i] != null && !this.get(set2.slots[i])) return false;
         }
         return true;
     }
